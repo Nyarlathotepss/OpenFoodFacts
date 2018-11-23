3cc0630fbe0f4545a49e08af1762bbcd670b9446
Nyarlathotepss <rcp.nmillet@gmail.com>
2018-11-23 10:01:35 +0100

    docstring ajouté

--- Elements.py
+++ Elements.py
@@ -1,22 +1,18 @@
 import requests
 
 class API :
-
+'''Communicate with openfoodfact API / Informations will be in variable "text" '''
     def __init__(self):
         self.text = str
 
     def communication_api(self,url):
         self.r = requests.get(''url'')
-        self.r = r.text
+        self.text = r.text
 
 class Display :
-
-    def disp_list(self,text):
+'''allow to display "text" from API '''
+    def __init__(self):
         self.list_info = []
-
-    def sort_info(self,text):
-        for word in text :
-            self.list_info.append(word) # a  titre d'exemple
 
     def print_info(self) :
         print(list_info)
